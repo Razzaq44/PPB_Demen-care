04e0c986a3b7df5085c68dc8c6696a8f68dff5e0
Razzaq Wibowo <razzaq.bowo@gmail.com>
2023-04-12 04:43:56 +0700

    fixing route on new component

--- lib/MedicalRecords.dart
+++ lib/MedicalRecords.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Medical Records',
-      theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
-      ),
-      home: MedicalRecordsPage(),
-    );
-  }
-}
-
 class MedicalRecords {
   final String doctor;
   final String patient;
@@ -27,7 +12,7 @@ class MedicalRecords {
 class MedicalRecordsPage extends StatefulWidget {
   const MedicalRecordsPage({super.key});
 
-   @override
+  @override
   State<MedicalRecordsPage> createState() => _MedicalRecordsPageState();
 }
 
